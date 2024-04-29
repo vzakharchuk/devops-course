c1a20e94071600504f983cfe1c7246a1d9bab9fe
Vadym Zakharchuk <zakharchuk.vadym@gmail.com>
2024-04-29 20:43:48 +0200

    updated version to v7

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Define a route to handle GET requests to the root URL
 app.get('/', (req, res) => {
-    res.send('Hello there: v6');
+    res.send('Hello there: v7');
 });
 
 // Start the server listening on port 3000
